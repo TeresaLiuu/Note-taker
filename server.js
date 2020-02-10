f31935b59118c822403f88df19d35a67f9820bb8
Teresa Liu <kuas033@gmail.com>
2020-02-09 21:12:12 -0800

    get css path

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 //routes
+app.get('/assets/css/styles.css', (req, res) =>
+  res.sendFile(path.join(__dirname, './assets/css/styles.css'))
+);
+
+app.get('/assets/js/index.js', (req, res) =>
+  res.sendFile(path.join(__dirname, './assets/js/index.js'))
+);
+
 app.get('/', (req,res) =>{
     const absolutePath = path.join(__dirname, './assets/index.html');
     res.sendFile(absolutePath);
@@ -22,25 +30,26 @@ app.get('/notes', (req,res)=>{
 });
 
 app.get('/api/notes', (req,res) =>{
-    let notes;
+    let notes = '';
     fs.readFile('db.json','utf8',(err,data)=>{
         if (err) throw err;
-        notes = data;
-        res.json(JSON.parse(notes));
+        
     });
 });
 
 app.post ('/api/notes', (req,res) =>{
     const newNotes = req.body;
-    let notes;
-    fs.readFile('bd.json', 'utf8', (err,data)=>{
+    let notes = '';
+    fs.readFile('./db.json', 'utf8', (err,data)=>{
         if (err) throw err;
         notes = JSON.parse (data);
-        fs.writeFile('bd.json', JSON.stringify(notes),'utf8',err=>{
+        notes.push(newNotes);
+        fs.writeFile('./db.json', JSON.stringify(notes),'utf8',err=>{
             if (err) throw 'something is wrong';
         });
         
-    })
+    });
+    res.sendFile(path.join(__dirname, './assets/notes.html'));
 })
 
 
