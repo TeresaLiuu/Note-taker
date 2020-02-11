0ab7625623a2c1378044dbcc82a0759a50d9b8e2
Teresa Liu <kuas033@gmail.com>
2020-02-10 20:27:07 -0800

    get data write to db.json file

--- server.js
+++ server.js
@@ -9,52 +9,53 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use('/assets', express.static(__dirname + '/assets'));
 
 //routes
-app.get('/assets/css/styles.css', (req, res) =>
-  res.sendFile(path.join(__dirname, './assets/css/styles.css'))
-);
-
-app.get('/assets/js/index.js', (req, res) =>
-  res.sendFile(path.join(__dirname, './assets/js/index.js'))
-);
 
 app.get('/', (req,res) =>{
-    const absolutePath = path.join(__dirname, './assets/index.html');
-    res.sendFile(absolutePath);
+    res.sendFile(path.join(__dirname, './assets/index.html'));
 });
 
 app.get('/notes', (req,res)=>{
-    const absolutePath = path.join(__dirname, './assets/notes.html');
-    res.sendFile(absolutePath);
+    res.sendFile(path.join(__dirname, './assets/notes.html'));
 });
 
 app.get('/api/notes', (req,res) =>{
-    let notes = '';
-    fs.readFile('db.json','utf8',(err,data)=>{
-        if (err) throw err;
-        
+    let note = '';
+    fs.readFile('./db.json','utf8',(err,data)=>{
+        if (err) throw err; 
+        note = JSON.parse (data);
+        fs.writeFile('./db.json', JSON.stringify(note),'utf8',err=>{
+            if (err) throw 'something is wrong';
+        });   
     });
 });
 
 app.post ('/api/notes', (req,res) =>{
-    const newNotes = req.body;
-    let notes = '';
+    const newNote = req.body;
+    let note = '';
     fs.readFile('./db.json', 'utf8', (err,data)=>{
         if (err) throw err;
-        notes = JSON.parse (data);
-        notes.push(newNotes);
-        fs.writeFile('./db.json', JSON.stringify(notes),'utf8',err=>{
+        note = JSON.parse (data);
+        note.push(newNote);
+        fs.writeFile('./db.json', JSON.stringify(note),'utf8',err=>{
             if (err) throw 'something is wrong';
         });
         
     });
     res.sendFile(path.join(__dirname, './assets/notes.html'));
-})
+});
 
 
 
 
 
-//listener
+
+
+// //listener
 app.listen(PORT,() => console.log(`App listening on port ${PORT}`));
+
+
+
+
